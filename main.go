fa00824fe29e51b58cfaf302a4adfe682a981c76
Troy Dai <git@troydai.com>
2024-11-28 13:02:40 -0800

    add logs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 	"os"
 	"strconv"
@@ -8,16 +9,21 @@ import (
 )
 
 func (s *server) handler(w http.ResponseWriter, r *http.Request) {
+	s.logger.Info("inbound", "method", r.Method, "url", r.URL.Path, "host", r.Host, "proto", r.Proto, "agent", r.UserAgent(), "peer", r.RemoteAddr, "counter", s.counter.Load())
+
 	if newValue := s.counter.Add(1); newValue%s.frequency == 0 {
+		s.logger.Warn("crash", "counter", newValue)
 		panic("crash after 10 requests")
 	}
 
+	s.logger.Info("outbound", "status", 200)
 	w.WriteHeader(200)
 }
 
 type server struct {
 	counter   *atomic.Uint64
 	frequency uint64
+	logger    *slog.Logger
 }
 
 func main() {
@@ -30,6 +36,7 @@ func main() {
 	s := &server{
 		counter:   &atomic.Uint64{},
 		frequency: frequency,
+		logger:    slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
 	http.HandleFunc("/", s.handler)
